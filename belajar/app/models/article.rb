6cda7ddc76f7742e74814440200523790cecc716
varid007 <sadewa44@gmail.com>
2012-05-03 10:16:56 +0700

    revisi code on task 3

--- belajar/app/models/article.rb
+++ belajar/app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ActiveRecord::Base
   belongs_to :user
 
   scope :rating_is_and_above, lambda {|rate| where("rating > ?", rate) }
+  validates :title, :uniqueness => true,
+                    :presence => true, 
+                    :length => {:minimum => 1, :maximum => 15},
+                    :format => {:with => /[a-zA-Z\s]+$/}
 end
--- belajar/app/models/user.rb
+++ belajar/app/models/user.rb
@@ -3,9 +3,28 @@ class User < ActiveRecord::Base
   has_many :articles
   has_many :countries
 
+  has_many :shared_articles,
+           :class_name => "Article",
+           :foreign_key => "user_id"
+           :"title like '%share%'"
+         
   def show_full_name
     
     "#{self.first_name} #{self.last_name}"
   end
-
+  validates :first_name, :presence => true,
+            :length => {:minimum => 1, :maximum => 20},
+            :format => {:with => /[a-zA-Z\s]+$/}
+  validates :last_name, :presence => true,
+            :length => {:minimum => 1, :maximum => 20},
+            :format => {:with => /[a-zA-Z\s]+$/}
+              
+  validates :email, :presence => true,
+                    :length => {:minimum => 3, :maximum => 254},
+                    :uniqueness => true,
+                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}  
+   validates :username, :presence => true,
+             :length => {:minimum => 3, :maximum => 254},
+             :uniqueness => true,
+             :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
